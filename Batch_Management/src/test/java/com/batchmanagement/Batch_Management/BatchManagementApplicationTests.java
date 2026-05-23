38a2d0789276ad04782de64346bd0326bce5fbab
Vaibhavi-06 <fulsundarvaibhavi1@gmail.com>
2026-05-23 20:30:40 +0530

    Initial Spring Boot setup

---
+++ Batch_Management/src/test/java/com/batchmanagement/Batch_Management/BatchManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batchmanagement.Batch_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
